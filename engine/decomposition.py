4688f85c3c07d01b60ef6174ce9efdf12df2ff63
trhoangdung <trhoangdung@gmail.com>
2017-12-20 14:47:56 -0600

    add init file and start working on Marz decoupling

--- engine/decomposition.py
+++ engine/decomposition.py
@@ -3,6 +3,8 @@ This module implements decomposition techniques for high-index DAE
 Dung Tran: Dec/2017
 '''
 
+from engine.dae_automaton import DaeAutomation
+
 
 class Decomposition(object):
     'Decomposition techniques for high-index DAE'
@@ -13,5 +15,16 @@ class Decomposition(object):
 
         pass
 
+    @staticmethod
+    def Marz_decoupling_index_1(dae_automaton):
+        'implement Marz decoupling for index 1 dae automaton'
 
-if __name__ == '__main__':
+        # computation approach is based on the paper:
+        # "An efficient projector-based passivity test for descritpor", Z.Zhang and N.Wong, 2010
+
+        assert isinstance(dae_automaton, DaeAutomation)
+
+        matrix_e = dae_automaton.matrix_e
+        matrix_a = dae_automaton.matrix_a
+        matrix_b = dae_automaton.matrix_b
+        matrix_c = dae_automaton.matrix_c

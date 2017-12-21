c3e3a411fdddb44f1d1046978e2dcae2dea0a790
trhoangdung <trhoangdung@gmail.com>
2017-12-21 15:53:19 -0600

    test get null space of a matrix

--- engine/canonical_projectors.py
+++ engine/canonical_projectors.py
@@ -4,7 +4,8 @@ Dung Tran: Dec/2017
 '''
 
 from scipy.sparse import issparse, random
-from scipy.sparse.linalg import splu, svds
+from scipy.linalg import svd
+import time
 import numpy as np
 
 
@@ -38,21 +39,31 @@ def luq(matrix_a, tol):
 
 if __name__ == '__main__':
 
-    matrix_a = random(10, 10, density=0.1, format='csc')
+    matrix_a = random(6, 6, density=0.1, format='csc')
+    # dim = 1000, few seconds
+    # dim = 2000, 20 seconds
+    # dim = 4000, 182 seconds
+
+    # accuracy is arround e-12
 
     print "\nmatrix_a = \n{}".format(matrix_a.todense())
 
+    r = np.linalg.matrix_rank(matrix_a.todense())
+
+    print "\nrank of matrix_a = {}".format(r)
     m, n = matrix_a.shape
 
     print "\nm = {}, n = {}".format(m, n)
 
-    k = min(matrix_a.shape)
-
-    print "\nk= {}".format(k)
+    if m != n:
+        raise ValueError('not a square matrix')
 
     # test svd decomposition
+    start = time.time()
+    u, s, vt = svd(matrix_a.todense())
+    end = time.time()
+    print "\nsvd computation time = {}".format(end - start)
 
-    u, s, vt = svds(matrix_a, k - 1, tol=1.0e-12)
     matrix_s = np.diag(s)
     us = np.dot(u, matrix_s)
     usvt = np.dot(us, vt)
@@ -61,10 +72,16 @@ if __name__ == '__main__':
 
     print "\nusvt = \n{}".format(usvt)
 
-    print "\nusvt - matrix_a = {}".format(usvt - matrix_a)
-
     print "\nnorm of usvt - matrix_a = {}".format(np.linalg.norm(usvt - matrix_a))
 
-    # test lu decomposition
+    Q = vt[r:n, :]
+    null_a = np.transpose(Q)
 
-    #lu = splu(matrix_a)
+    print "\nnull space of matrix _a = spane of {}".format(null_a)
+    if null_a != []:
+
+        print "\nnorm of  matrix_a * null_1 = {}".format(np.linalg.norm(np.multiply(matrix_a.todense(), null_a[:, 0])))
+        print "\nnorm of matrix_a * null_2 = {}".format(np.linalg.norm(np.multiply(matrix_a.todense(), null_a[:, 1])))
+
+    else:
+        print "\nmatrix_a is full rank"

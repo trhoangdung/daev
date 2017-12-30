1bddbe06d7a4fe111fc4bfaf09813b6776b5ada9
trhoangdung <trhoangdung@gmail.com>
2017-12-29 22:22:46 -0600

    finish ode_sim

--- engine/reachability.py
+++ engine/reachability.py
@@ -3,13 +3,11 @@ This modules implements simulation-based reachability analysis for decoupled dae
 Dung Tran: Dec/2017
 '''
 
+import time
 import numpy as np
 #from engine.set import ReachSet
 from scipy.integrate import ode
 from scipy.sparse import csc_matrix
-import time
-from sympy import Matrix, Symbol, Function, symarray, lambdify
-
 
 class ReachSetAssembler(object):
     'implements rechable set computation for odes, daes'
@@ -20,35 +18,28 @@ class ReachSetAssembler(object):
         # solvers can be selected from the list = ['vode', 'zvode',  'Isoda', 'dopri5', 'dop853']
 
         start = time.time()
-        assert isinstance(matrix_a, np.ndarray) or isinstance(matrix_a, csc_matrix)
+        assert isinstance(matrix_a, np.ndarray), 'error: matrix_a is not ndarray'
         if solver_name != 'vode' and solver_name != 'zvode' and solver_name != 'Isoda' and solver_name != 'dopri5' and solver_name != 'dop853':
             raise ValueError('error: invalid solver name')
 
         n = matrix_a.shape[0]
 
-        def fun(y, _):
-            y = Matrix.zeros(n, 1)
-            for i in xrange(0, n):
-                y[i, 0] = Symbol('y[{}]'.format(i))
-
-            print y
-            rv = matrix_a * y
-            print "\nrv = {}".format(rv)
-            print "\nshape of rv = {}".format(rv.shape)
+        def fun(t, y):
+            'derivative function'
+            rv = np.dot(matrix_a, y)
             return rv
 
         t0 = 0.0
-        t = np.linspace(t0, totime, num_steps)
+        t = np.linspace(t0, totime, num_steps + 1)
 
         solver = ode(fun)
         solver.set_integrator(solver_name)
         solver.set_initial_value(init_vec, t0)
-        sol = np.empty((num_steps, n))
+        sol = np.empty((num_steps + 1, n))
         sol[0] = init_vec
 
         k = 1
         while solver.successful() and solver.t < totime:
-            print "\nt[{}] = {}".format(k, t[k])
             solver.integrate(t[k])
             sol[k] = solver.y
             k += 1

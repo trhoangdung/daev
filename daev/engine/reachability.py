ef1fdb022f4d46308f36750560ea218acf456bb4
trhoangdung <trhoangdung@gmail.com>
2018-01-02 15:48:25 -0600

    finish reachable set computation for autonomous ode, working toward reachable set computation for dae

--- daev/engine/reachability.py
+++ daev/engine/reachability.py
@@ -5,9 +5,9 @@ Dung Tran: Dec/2017
 
 import time
 import numpy as np
-from daev.engine.set import ReachSet
+from daev.engine.set import ReachSet, InitSet
 from scipy.integrate import ode
-from scipy.sparse import csc_matrix
+
 
 class ReachSetAssembler(object):
     'implements rechable set computation for odes, daes'
@@ -15,10 +15,12 @@ class ReachSetAssembler(object):
     @staticmethod
     def ode_sim(matrix_a, init_vec, totime, num_steps, solver_name):
         'compute simulation trace of an ode'
-        # solvers can be selected from the list = ['vode', 'zvode',  'Isoda', 'dopri5', 'dop853']
+        # solvers can be selected from the list = ['vode', 'zvode',  'Isoda',
+        # 'dopri5', 'dop853']
 
         start = time.time()
-        assert isinstance(matrix_a, np.ndarray), 'error: matrix_a is not ndarray'
+        assert isinstance(
+            matrix_a, np.ndarray), 'error: matrix_a is not ndarray'
         if solver_name != 'vode' and solver_name != 'zvode' and solver_name != 'Isoda' and solver_name != 'dopri5' and solver_name != 'dop853':
             raise ValueError('error: invalid solver name')
 
@@ -48,22 +50,50 @@ class ReachSetAssembler(object):
         return sol, runtime
 
     @staticmethod
-    def reach_autonomous_ode(matrix_a, init_reachset, step, num_steps, solvers):
+    def reach_autonomous_ode(matrix_a, init_reachset,
+                             totime, num_steps, solver_name):
         'compute reachable set of automnous linear ode: \dot{x} = Ax'
 
         # compute reachable set using simulation
-        # solvers can be selected from the list = ['vode', 'zvode',  'Isoda', 'dopri5', 'dop853']
-
-        assert isinstance(matrix_a, np.ndarray) and matrix_a.shape[0] == matrix_a.shape[1], 'error: invalid matrix a'
+        # solvers can be selected from the list = ['vode', 'zvode',  'Isoda',
+        # 'dopri5', 'dop853']
+        start = time.time()
+        assert isinstance(
+            matrix_a, np.ndarray) and matrix_a.shape[0] == matrix_a.shape[1], 'error: invalid matrix a'
         assert isinstance(init_reachset, ReachSet)
         assert matrix_a.shape[0] == init_reachset.S.shape[0], 'error: inconsistent matrix_a and initial reach set'
-        assert isinstance(step, float) and step > 0, 'error: invalid step size'
-        assert isinstance(num_steps, int) and num_steps >= 0, 'error: invalid number of steps'
+        assert isinstance(totime, float) and totime > 0, 'error: invalid time'
+        assert isinstance(
+            num_steps, int) and num_steps >= 0, 'error: invalid number of steps'
 
-        pass
+        matrix_S = init_reachset.S
+        alpha_min = init_reachset.alpha_min_vec
+        alpha_max = init_reachset.alpha_max_vec
+        n, k = matrix_S.shape
+        sol_list = []
+
+        for j in xrange(0, k):
+            init_vec = matrix_S[:, j]
+            sol, _ = ReachSetAssembler().ode_sim(matrix_a, init_vec, totime, num_steps, solver_name)
+            sol_list.append(sol)
+
+        reach_set_list = []
+        for i in xrange(0, num_steps):
+            reach_set = ReachSet()
+            s_mat = np.empty((n, k))
+            for j in xrange(0, k):
+                sol = sol_list[j]
+                s_mat[:, j] = np.transpose(sol[i])
+
+            reach_set.set_params(s_mat, alpha_min, alpha_max)
+            reach_set_list.append(reach_set)
+
+        runtime = time.time() - start
+
+        return reach_set_list, runtime
 
 
-def test():
+def test_ode_sim():
     'test methods'
 
     A = np.array([[0.0, -1.0], [-1.0, 0.0]])
@@ -74,9 +104,25 @@ def test():
     solution, runtime = ReachSetAssembler().ode_sim(A, init_vec, final_time, num_steps, 'dopri5')
 
     print "\nsolution = {}".format(solution)
-
     print "\nruntime = {}".format(runtime)
 
 
+def test_reach_autonomous_ode():
+    'test reach_autonomous_ode method'
+
+    A = np.array([[0.0, -1.0], [-1.0, 0.0]])
+    min_vec = [0.0, 0.1]
+    max_vec = [0.0, 0.2]
+    init_cond = InitSet()
+    init_cond.set_params(min_vec, max_vec)
+    initSet = init_cond.get_init_reachset()
+    totime = 2.0
+    num_steps = 20
+    solver_name = 'dopri5'
+    reach_set_list, runtime = ReachSetAssembler.reach_autonomous_ode(A, initSet, totime, num_steps, solver_name)
+    print "\nreachable set computation runtime = {}".format(runtime)
+    print "\nreachable set list = {}".format(reach_set_list)
+
 if __name__ == '__main__':
-    test()
+    #test_ode_sim()
+    test_reach_autonomous_ode()
